3e6cdd18e3165ff64ef775de50716611cd2955e6
ChrisHopwood <chrishopwood@outlook.com>
2022-08-02 16:58:17 +0100

    this works now

--- app.py
+++ app.py
@@ -1,25 +1,32 @@
 from flask import Flask
 from datetime import datetime
-
 from flask import render_template, request
+
+
+shopOpen = '09:00:00'
+shopClose = '17:00:00'
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+myOpenClosedState = ''
 
 app = Flask(__name__)
 def myConvert(amount):
     return amount * 1.18
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
 @app.route('/result', methods=['GET', 'POST'])
 def result():
-    myformData = request.form.get('pounds')
-    myformData = int(myformData)
-    myConversion = myConvert(myformData)
+  
+  if (current_time > shopOpen) and (current_time < shopClose):
+    myOpenClosedState = 'open'
+  else:
+    myOpenClosedState = 'closed'
 
-    myDTnow = datetime.now()
+  return render_template('result.html', resultOpenClosedState=myOpenClosedState, resultDTnow=current_time)
 
 
-
-
-    return render_template('result.html', myformData=myformData, myConversion=myConversion)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000', debug=True)
