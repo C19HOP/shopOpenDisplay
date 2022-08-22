744db8d9e3ee8350767e62a73ff4e30043973358
ChrisHopwood <chrishopwood@outlook.com>
2022-08-22 12:12:46 +0100

    reworked function

--- app.py
+++ app.py
@@ -3,30 +3,34 @@ from datetime import datetime
 from flask import render_template, request
 
 
-shopOpen = '09:00:00'
-shopClose = '17:00:00'
+shopOpenTime = '09:00:00'
+shopCloseTime = '17:00:00'
 now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-myOpenClosedState = ''
+currentTime = now.strftime("%H:%M:%S")
+
 
 app = Flask(__name__)
-def myConvert(amount):
-    return amount * 1.18
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
+
+#function to render result
 @app.route('/result', methods=['GET', 'POST'])
 def result():
-  
-  if (current_time > shopOpen) and (current_time < shopClose):
-    myOpenClosedState = 'open'
-  else:
-    myOpenClosedState = 'closed'
+  thisShopOpenClosed = isTheCurrentlyShopOpen(shopOpenTime, shopCloseTime, currentTime)
+  return render_template('result.html', resultOpenClosedState=thisShopOpenClosed, resultDTnow=currentTime)
 
-  return render_template('result.html', resultOpenClosedState=myOpenClosedState, resultDTnow=current_time)
+
+#Function to check if the shop is open
+def isTheCurrentlyShopOpen(thisShopOpenTime, thisShopCloseTime, thisCurrentTime):
+  if (thisCurrentTime > thisShopOpenTime) and (thisCurrentTime < thisShopCloseTime):
+    return 'open'
+  else:
+    return 'closed'
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000', debug=True)
+
